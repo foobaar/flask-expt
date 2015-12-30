f8acd5e7759c435f014cbc6968ecf0581d17075c
baargav <you@example.com>
2015-12-30 19:18:15 +0000

    added f->c conversion

--- flask-experiment.py
+++ flask-experiment.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-  return 'Hello World!'
+def documenation():
+  return render_template('docs.html')
+
+@app.route('/celsius', methods=['GET'])
+def fahrenheit_to_celsius():
+  fahrenheit = request.args['fahrenheit']
+  is_input_a_number = is_number(fahrenheit)
+
+  if is_input_a_number:
+    celsius = (float() -32) * 5/9
+    return 'celsius: ' + str(celsius)
+  else:
+    return "Invalid input: " + fahrenheit;
+
+
+def is_number(s):
+  try:
+    float(s)
+    return True
+  except ValueError:
+    return False
 
 if __name__ == '__main__':
-  app.run()
+  app.run(debug = True)
